7a2d92ed23a2e7e8cf0d25f1432fa926b38a40ed
Lachlan Pham <44896700+lache-moi@users.noreply.github.com>
2021-09-29 05:31:16 +0400

    recompleted upto 2.2

--- createPubSchema.sql
+++ createPubSchema.sql
@@ -1,54 +1,69 @@
 create table author (
-   id int not null,
-   name text not null,
-   homepage text not null,
+   id int,
+   name text,
+   homepage text,
    CONSTRAINT author_pk PRIMARY KEY (id)
 );
  
 create table publication (
-   pubid int not null,
-   pubkey text not null,
-   title text not null,
-   year int not null,
-   type text not null,
+   pubid int,
+   pubkey text,
+   title text,
+   year int,
+   type text,
    CONSTRAINT publication_pk PRIMARY KEY (pubid),
    CONSTRAINT publication_ak UNIQUE (pubkey)
 );
  
 create table article (
-   pubid int not null,
-   journal text not null,
-   month int not null,
-   volume text not null,
-   number int not null,
+   pubid int,
+   pubkey text,
+   title text,
+   year int,
+   journal text,
+   month int,
+   volume text,
+   number int,
    FOREIGN KEY (pubid) REFERENCES publication (pubid)
 );
  
 create table book (
-   pubid int not null,
-   publisher text not null,
-   isbn int not null,
+   pubid int,
+   pubkey text,
+   title text,
+   year int,
+   publisher text,
+   isbn int,
    FOREIGN KEY (pubid) REFERENCES publication (pubid)
 );
  
 create table incollection (
-   pubid int not null,
-   booktitle text not null,
-   publisher text not null,
-   isbn int not null,
+   pubid int,
+   pubkey text,
+   title text,
+   year int,
+   booktitle text,
+   publisher text,
+   isbn int,
    FOREIGN KEY (pubid) REFERENCES publication (pubid)
 );
  
 create table inproceedings (
-   pubid int not null,
-   booktitle text not null,
-   editor text not null,
+   pubid int,
+   pubkey text,
+   title text,
+   year int,
+   booktitle text,
+   editor text,
    FOREIGN KEY (pubid) REFERENCES publication (pubid)
 );
 
 create table authored (
-   id int not null,
-   pubid int not null,
+   id int,
+   pubkey text,
+   title text,
+   year int,
+   pubid int,
    FOREIGN KEY (id) REFERENCES author (id),
    FOREIGN KEY (pubid) REFERENCES publication (pubid)
 );
